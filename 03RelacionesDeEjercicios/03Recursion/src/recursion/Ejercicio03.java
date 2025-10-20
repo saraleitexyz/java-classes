f90de8588c323d1826656e759e58c7b463e454a1
Sara Leite <saraleite.mail@gmail.com>
2025-10-20 22:58:12 +0200

    Relacion 3 ex 03

--- 03RelacionesDeEjercicios/03Recursion/src/recursion/Ejercicio03.java
+++ 03RelacionesDeEjercicios/03Recursion/src/recursion/Ejercicio03.java
@@ -1,5 +1,56 @@
 package recursion;
 
+/*
+3. División por Restas Sucesivas:
+Implementar un algoritmo recursivo que permita realizar la división entre
+dos números usando restas sucesivas.
+*/
+
+import java.util.Scanner;
+
 public class Ejercicio03 {
 
+    // 🔹 División iterativa
+    public static int divIte(int dividendo, int divisor) {
+        int cociente = 0;
+        
+        while (dividendo >= divisor) {
+            dividendo -= divisor;
+            cociente++;
+        }
+
+        System.out.println("División iterativa: " + cociente);
+        return cociente;
+    }
+
+    // 🔹 División recursiva
+    public static int divRecu(int dividendo, int divisor) {
+        if (dividendo < divisor) {
+            return 0;
+        } else {
+            return 1 + divRecu(dividendo - divisor, divisor);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("DIVISIÓN POR RESTAS SUCESIVAS");
+        System.out.println("------------------------------");
+
+        System.out.print("Introduce el dividendo: ");
+        int dividendo = reader.nextInt();
+
+        System.out.print("Introduce el divisor: ");
+        int divisor = reader.nextInt();
+
+        if (divisor == 0) {
+            System.out.println("Error: No se puede dividir entre 0.");
+        } else {
+            divIte(dividendo, divisor);
+            System.out.println("División recursiva: " + divRecu(dividendo, divisor));
+        }
+
+        reader.close();
+    }
 }

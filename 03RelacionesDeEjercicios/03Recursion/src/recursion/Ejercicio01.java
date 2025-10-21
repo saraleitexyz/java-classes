691de9303018e833ea44f48ed4ef9277b71999c8
SaraExamen <emailSara@examen.com>
2025-10-21 08:54:43 +0200

    Added relation 03 exercise 01

--- 03RelacionesDeEjercicios/03Recursion/src/recursion/Ejercicio01.java
+++ 03RelacionesDeEjercicios/03Recursion/src/recursion/Ejercicio01.java
@@ -1,5 +1,32 @@
 package recursion;
 
+/*
+1. Potencia de un Número: Diseñar un método recursivo que calcule la 
+potencia de un número real x elevado a un exponente entero n.
+*/
+
 public class Ejercicio01 {
+	public static long potiter(int num, int pot) {
+		int total = 1;
+		for (int i = 0; i < pot; i++) {
+			total = total * num;
+		}
+		return total;
+	}
+	
+	public static int potrec(int num, int pot) {
+		// Caso base:
+		if (pot == 0) {
+			return 1;
+		// Caso general:
+		} else {
+			return (num * potrec(num, pot - 1));
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(potiter(5,9));
+		System.out.println(potrec(5,9));
+	}
 
 }
